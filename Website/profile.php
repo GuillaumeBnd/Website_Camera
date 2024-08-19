a45b7fd5927ad85734660d09a77a297f6bf6b454
GuillaumeBnd <48201932+GuillaumeBnd@users.noreply.github.com>
2024-08-19 17:38:22 +0200

    Update profile.php
    Modify access to database

--- Website/profile.php
+++ Website/profile.php
@@ -12,7 +12,7 @@ if (!isset($_SESSION['loggedin'])) {
 }
 $DATABASE_HOST = 'localhost';
 $DATABASE_USER = 'root';
-$DATABASE_PASS = 'Dommartin69§';
+$DATABASE_PASS = '********';
 $DATABASE_NAME = 'Website_Camera';
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if (mysqli_connect_errno()) {
